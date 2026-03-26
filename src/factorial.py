28f0498cd5dd3e2317909fe24429419a1ca129e4
BiancaTournour <bmtournour@gmail.com>
2026-03-26 10:18:45 -0300

    modificacion de la funcion factorial y agregado de comentarios

--- src/factorial.py
+++ src/factorial.py
@@ -1,5 +1,6 @@
 import sys
 
+#funcion de factorial
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
@@ -14,13 +15,24 @@ def factorial(num):
         return fact 
 
 if len(sys.argv) < 2:
-    rango = input("Ingrese un rango (ej. 4-8): ")
+    rango = input("Ingrese un rango (ej. 4-8, -10, 4-): ")
 else:
     rango = sys.argv[1]
 
 partes = rango.split("-")
-desde = int(partes[0])
-hasta = int(partes[1])
+
+if partes[0] == "":
+    # Caso "-hasta": desde 1 hasta el número indicado
+    desde = 1
+    hasta = int(partes[1])
+elif partes[1] == "":
+    # Caso "desde-": desde el número indicado hasta 60
+    desde = int(partes[0])
+    hasta = 60
+else:
+    # Caso normal "desde-hasta"
+    desde = int(partes[0])
+    hasta = int(partes[1])
 
 for num in range(desde, hasta + 1):
     print("Factorial ", num, "! es ", factorial(num))
