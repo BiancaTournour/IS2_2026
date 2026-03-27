fa98cbf346ab7371336c28182cd785d2b739f4e2
BiancaTournour <bmtournour@gmail.com>
2026-03-27 09:44:18 -0300

    modificación conjetura de collatz

--- TP1/src/Collatz/collatz.py
+++ TP1/src/Collatz/collatz.py
@@ -14,10 +14,9 @@ numeros = list(range(1, 10001))
 iteraciones = [collatz_iteraciones(n) for n in numeros]
 
 plt.figure(figsize=(12, 6))
-plt.plot(numeros, iteraciones, linewidth=0.5, color="steelblue")
+plt.plot(numeros, iteraciones, 'o', markersize=0.5, color="steelblue")
 plt.xlabel("Número de iteraciones hasta converger")
 plt.ylabel("Número n de inicio")
 plt.title("Conjetura de Collatz - números del 1 al 10000")
 plt.tight_layout()
-plt.savefig("src/collatz/collatz.png")
 plt.show()
